1916d553a0052d0926caf0f38d7ebc50c182fcc9
chauvo <itoutsource03@generali-life.com.vn>
2020-03-11 10:39:35 +0700

    update file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 require('./models/User')
+require('./models/Track')
 
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const authRoutes = require('./routes/authRoutes')
+const trackRoutes = require('./routes/trackRoutes')
 const requireAuth = require('./middlewares/requireAuth')
 
 const app = express()
@@ -11,6 +13,7 @@ const app = express()
 app.use(bodyParser.json())
 
 app.use(authRoutes)
+app.use(trackRoutes)
 
 const mongoUri = 'mongodb+srv://admin:passwordpassword@cluster0-2wsg9.mongodb.net/test?retryWrites=true&w=majority'
 mongoose.connect(mongoUri, {
